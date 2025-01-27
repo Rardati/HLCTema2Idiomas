f191af2eba716942e7cc39ec9ec71f9123188928
Rardati <rardati601@g.educaand.es>
2025-01-27 14:33:42 +0100

    Guardar

--- src/app/detalle/detalle.page.ts
+++ src/app/detalle/detalle.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallePage implements OnInit {
 
-  constructor() { }
+  id:string = "";
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    let idRecibido = this.activatedRoute.snapshot.paramMap.get('id');
+    if(idRecibido != null){
+      this.id = idRecibido;
+    }else{
+      this.id = "";
+    }
   }
 
 }
