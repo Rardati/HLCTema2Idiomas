bc08c352533627f4603802103e394bc39794d5b0
Rardati <rardati601@g.educaand.es>
2025-02-05 13:11:28 +0100

    Guardar

--- src/app/detalle/detalle.page.ts
+++ src/app/detalle/detalle.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FirestoreService } from './firestore.service';
+import { FirestoreService } from '../firestore.service';
 import { Idiomas } from '../idiomas';
 
 @Component({
