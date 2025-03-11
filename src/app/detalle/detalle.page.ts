a43b14550575cd1ab34824d0bf7e0f6281ccc806
Rardati <rardati601@g.educaand.es>
2025-03-11 10:13:12 +0100

    Guardar

--- src/app/detalle/detalle.page.ts
+++ src/app/detalle/detalle.page.ts
@@ -15,15 +15,16 @@ export class DetallePage implements OnInit {
   id: string = "";
   idioma: any = { data: {} };
   nuevoDato: boolean = false;
+  imagenSelect: string = "";
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private firestoreService: FirestoreService, 
+    private firestoreService: FirestoreService,
     private router: Router,
     private alertController: AlertController,
     private loadingController: LoadingController,
     private toastController: ToastController,
-    private imagePickerr: ImagePicker
+    private imagePicker: ImagePicker
 
   ) {
     this.idioma.data = {} as Idiomas;
@@ -36,13 +37,13 @@ export class DetallePage implements OnInit {
       this.nuevoDato = this.id === "nuevo";
 
       if (!this.nuevoDato) {
-        this.firestoreService.consultarPorId("idiomas_raquel", this.id).subscribe((resultado: any) => {    
-          if (resultado.payload.data()) {          
+        this.firestoreService.consultarPorId("idiomas_raquel", this.id).subscribe((resultado: any) => {
+          if (resultado.payload.data()) {
             this.idioma.id = resultado.payload.id;
-            this.idioma.data = resultado.payload.data();    
-          } else {          
+            this.idioma.data = resultado.payload.data();
+          } else {
             this.idioma.data = {} as Idiomas;
-          } 
+          }
         });
       }
     }
@@ -67,7 +68,8 @@ export class DetallePage implements OnInit {
         message: '¿Estás seguro de que deseas eliminar este idioma?',
         buttons: [
           { text: 'Cancelar', role: 'cancel' },
-          { text: 'Eliminar', handler: () => {
+          {
+            text: 'Eliminar', handler: () => {
               this.firestoreService.borrar("idiomas_raquel", this.id).then(() => {
                 this.router.navigate(["/home"]);
               });
@@ -79,4 +81,81 @@ export class DetallePage implements OnInit {
       await alert.present();
     }
   }
-}
+
+
+
+  async seleccionaImagen() {
+
+    this.imagePicker.hasReadPermission().then(
+      (result) => {
+        if (result == false) {
+          this.imagePicker.requestReadPermission();
+        }
+        else {
+          this.imagePicker.getPictures({
+            maximumImagesCount: 1,
+            outputType: 1
+          }).then(
+            (results) => {
+              if (results.lenght > 0) {
+                this.imagenSelect = 'data:image/jpeg;base64,' + results[0];
+                console.log("Imagen que se ha seleccionado ( en Base 64):" + this.imagenSelect);
+              }
+            },
+            (err) => {
+              console.log(err);
+            }
+          );
+        }
+      },
+      (err) => {
+        console.log(err);
+      
+      });
+    }
+
+
+
+
+    async subirImagen() {
+      const loading = await this.loadingController.create({
+        message:'Please wait...'
+      });
+
+      const toast = await this.toastController.create({
+        message: 'Imagen subida correctamente',
+        duration: 3000
+      });
+
+      let nombreCarpeta = "imagenes_raquel";
+
+      loading.present();
+
+      let nombreImagen = `${new Date().getTime()}`;
+
+      this.firestoreService.subirImagenBase64(nombreCarpeta, nombreImagen, this.imagenSelect)
+      .then(snapshot => {
+        snapshot.ref.getDownloadURL()
+          .then(downloadURL => {
+            console.log("URL de descarga: " + downloadURL);
+            toast.present();
+            loading.dismiss();
+          })
+        })
+      }
+
+
+      async eliminarArchivo(fileURL:string) {
+        const toast = await this.toastController.create({
+          message:'File was deleted successfully',
+          duration: 3000
+        });
+        this.firestoreService.eliminarArchivoPorURL(fileURL)
+        .then(() => {
+          toast.present();
+        }, (error) => {
+          console.error(error);
+        })
+      }
+
+  }
